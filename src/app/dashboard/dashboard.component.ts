cc5d859339a5991442984b0267e6f4030f514676
CorentinBoutillier <corentinboutillier@gmail.com>
2018-07-08 16:52:28 +0000

    Dev (#22)
    * feat: Add bootstrap
    
    - Add bootstrap sass
    
    * feat: Add fonts
    
    - Add font awesome
    - Add flaticon
    - Add bootstrap
    
    * feat/prettier: Add Prettier
    
    * feat: Dashboard component
    
    - Add DashboardComponent
    - Add 404 component
    - Add Router
    
    * Feat/gridster (#21)
    
    * feat: Dashboard component
    
    - Add DashboardComponent
    - Add 404 component
    - Add Router
    
    * feat: Gridster
    
    - Add gridster

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardGridsterConfigService } from "./dashboard-gridster-config.service";
+import { GridsterConfig, GridsterItem } from "angular-gridster2";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public config: GridsterConfig;
+  public items: Array<GridsterItem>;
+  constructor(private dashboardGridsterConfigService: DashboardGridsterConfigService) { }
 
   ngOnInit() {
+    this.config = this.dashboardGridsterConfigService.getConfig();
+    this.items =[
+      {cols: 1, rows: 2, y: 0, x: 0},
+      {cols: 1, rows: 1, y: 0, x: 0},
+      {cols: 1, rows: 1, y: 0, x: 0},
+      {cols: 1, rows: 1, y: 0, x: 0},
+      {cols: 1, rows: 1, y: 0, x: 0},
+      {cols: 2, rows: 1, y: 0, x: 0},
+      {cols: 1, rows: 1, y: 0, x: 0},
+      {cols: 1, rows: 1, y: 0, x: 0},
+      {cols: 1, rows: 1, y: 0, x: 0},
+      {cols: 1, rows: 1, y: 0, x: 0},
+      {cols: 1, rows: 1, y: 0, x: 0}
+    ]
   }
-
+  
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import {DashboardRouteModule} from "./dashboard.route";
+import {GridsterModule} from "angular-gridster2";
+import {DashboardGridsterConfigService} from "./dashboard-gridster-config.service";
+
 
 @NgModule({
   imports: [
     CommonModule,
-    DashboardRouteModule
+    DashboardRouteModule,
+    GridsterModule
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent],
+  providers: [DashboardGridsterConfigService]
 })
+
 export class DashboardModule { }
