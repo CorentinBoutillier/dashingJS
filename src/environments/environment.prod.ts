567701ba7b789b8b5ca30e30f28e0e0472c36522
Corentin <corentinboutillier@gmail.com>
2018-07-08 21:17:55 +0200

    feat: Gridster
    - Add gridsterItems

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,32 @@
+import { DashingjsConfig } from '../app/dashboard/interfaces/dashingjs-config';
+
+const dashingjsConfig: DashingjsConfig = {
+  items: [
+    { cols: 1, rows: 2, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 2, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 2, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 },
+    { cols: 1, rows: 1, y: 0, x: 0 }
+  ]
+};
 export const environment = {
-  production: true
+  production: true,
+  dashingjsConfig: dashingjsConfig
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,25 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { DashingjsConfig } from '../app/dashboard/interfaces/dashingjs-config';
+
+const dashingjsConfig: DashingjsConfig = {
+  items: [
+    { cols: 1, rows: 2, y: 0, x: 0, widget: { icon: 'fa fa-magic', class: 'bg-primary' } },
+    { cols: 1, rows: 1, y: 0, x: 0, widget: { icon: 'fa fa-google', class: 'bg-secondary' } },
+    { cols: 1, rows: 1, y: 0, x: 0, widget: { icon: null, class: 'bg-success' } },
+    { cols: 1, rows: 1, y: 0, x: 0, widget: { icon: 'fi flaticon-youtube', class: 'bg-danger' } },
+    { cols: 1, rows: 1, y: 0, x: 0, widget: { icon: null, class: 'bg-warning' } },
+    { cols: 2, rows: 1, y: 0, x: 0, widget: { icon: null, class: 'bg-info' } },
+    { cols: 1, rows: 1, y: 0, x: 0, widget: { icon: null, class: 'bg-light text-dark' } },
+    { cols: 1, rows: 1, y: 0, x: 0, widget: { icon: 'fa fa-bitcoin', class: 'bg-dark' } },
+    { cols: 1, rows: 1, y: 0, x: 0, widget: { icon: 'fa fa-download', class: 'bg-white text-dark' } },
+    { cols: 1, rows: 1, y: 0, x: 0, widget: { icon: 'fi flaticon-github', class: 'bg-primary' } }
+  ]
+};
 export const environment = {
-  production: false
+  production: false,
+  dashingjsConfig: dashingjsConfig
 };
 
 /*
